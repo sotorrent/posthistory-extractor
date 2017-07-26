fd09167f35c263a99bb2f519030913a97a8d368d
Sebastian Baltes <research@sbaltes.com>
2017-07-26 18:33:11 +0200

    Add test case for rootPostBlockVersion

--- src/de/unitrier/st/soposthistory/tests/PostVersionHistoryTest.java
+++ src/de/unitrier/st/soposthistory/tests/PostVersionHistoryTest.java
@@ -1,43 +1,45 @@
 package de.unitrier.st.soposthistory.tests;
 
 import de.unitrier.st.soposthistory.blocks.CodeBlockVersion;
+import de.unitrier.st.soposthistory.blocks.PostBlockVersion;
 import de.unitrier.st.soposthistory.blocks.TextBlockVersion;
 import de.unitrier.st.soposthistory.version.PostVersion;
 import de.unitrier.st.soposthistory.version.PostVersionList;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class PostVersionHistoryTest {
 
-    private static PostVersionList p_1109108;
-    private static PostVersionList p_3145655;
-    private static PostVersionList p_9855338;
-    private static PostVersionList p_2581754;
-    private static PostVersionList p_20991163;
+    private static PostVersionList a_1109108;
+    private static PostVersionList a_3145655;
+    private static PostVersionList a_9855338;
+    private static PostVersionList a_2581754;
+    private static PostVersionList a_20991163;
+    private static PostVersionList a_3758880;
 
     @BeforeAll
     static void init() {
-        p_1109108 = new PostVersionList();
-        p_1109108.readFromCSV("testdata/", 1109108, 2);
-        p_3145655 = new PostVersionList();
-        p_3145655.readFromCSV("testdata/", 3145655, 2);
-        p_9855338 = new PostVersionList();
-        p_9855338.readFromCSV("testdata/", 9855338, 2);
-        p_2581754 = new PostVersionList();
-        p_2581754.readFromCSV("testdata/", 2581754, 2);
-        p_20991163 = new PostVersionList();
-        p_20991163.readFromCSV("testdata/", 20991163, 2);
+        a_1109108 = new PostVersionList();
+        a_1109108.readFromCSV("testdata/", 1109108, 2);
+        a_3145655 = new PostVersionList();
+        a_3145655.readFromCSV("testdata/", 3145655, 2);
+        a_9855338 = new PostVersionList();
+        a_9855338.readFromCSV("testdata/", 9855338, 2);
+        a_2581754 = new PostVersionList();
+        a_2581754.readFromCSV("testdata/", 2581754, 2);
+        a_20991163 = new PostVersionList();
+        a_20991163.readFromCSV("testdata/", 20991163, 2);
+        a_3758880 = new PostVersionList();
+        a_3758880.readFromCSV("testdata/", 3758880, 2);
     }
 
     @Test
     void testReadingPostHistory1109108() {
-        assertEquals(7, p_1109108.size());
+        assertEquals(7, a_1109108.size());
 
-        PostVersion version_7 = p_1109108.get(6);
+        PostVersion version_7 = a_1109108.get(6);
         assertEquals(3, version_7.getPostBlocks().size());
         assertEquals(2, version_7.getTextBlocks().size());
         assertEquals(1, version_7.getCodeBlocks().size());
@@ -62,9 +64,9 @@ class PostVersionHistoryTest {
 
     @Test
     void testReadingPostHistory3145655() {
-        assertEquals(7, p_3145655.size());
+        assertEquals(7, a_3145655.size());
 
-        PostVersion version_7 = p_3145655.get(6);
+        PostVersion version_7 = a_3145655.get(6);
         assertEquals(5, version_7.getPostBlocks().size());
         assertEquals(3, version_7.getTextBlocks().size());
         assertEquals(2, version_7.getCodeBlocks().size());
@@ -86,9 +88,9 @@ class PostVersionHistoryTest {
 
     @Test
     void testReadingPostHistory9855338() {
-        assertEquals(11, p_9855338.size());
+        assertEquals(11, a_9855338.size());
 
-        PostVersion version_11 = p_9855338.get(10);
+        PostVersion version_11 = a_9855338.get(10);
         assertEquals(3, version_11.getPostBlocks().size());
         assertEquals(2, version_11.getTextBlocks().size());
         assertEquals(1, version_11.getCodeBlocks().size());
@@ -110,13 +112,13 @@ class PostVersionHistoryTest {
 
     @Test
     void testReadingPostHistory2581754() {
-        assertEquals(8, p_2581754.size());
+        assertEquals(8, a_2581754.size());
 
-        PostVersion version_3 = p_2581754.get(2);
+        PostVersion version_3 = a_2581754.get(2);
         assertEquals(6, version_3.getPostBlocks().size());
         assertEquals(3, version_3.getTextBlocks().size());
         assertEquals(3, version_3.getCodeBlocks().size());
-        System.out.println("version 4 block 7: " + p_2581754.get(3).getPostBlocks().get(6).getContent());
+        System.out.println("version 4 block 7: " + a_2581754.get(3).getPostBlocks().get(6).getContent());
 
         CodeBlockVersion codeBlock_1 = version_3.getCodeBlocks().get(0);
         String[] lines = codeBlock_1.getContent().split("\n");
@@ -134,26 +136,26 @@ class PostVersionHistoryTest {
         }
         System.out.println("text block size: " + version_3.getTextBlocks().size());
 
-        System.out.println("text block version 8, block 7: " + p_2581754.get(7).getPostBlocks().get(6).getContent());
+        System.out.println("text block version 8, block 7: " + a_2581754.get(7).getPostBlocks().get(6).getContent());
 
-        // System.out.println("text block version 7, block 8: " + p_2581754.get(6).getPostBlocks().get(7).getContent());
+        // System.out.println("text block version 7, block 8: " + a_2581754.get(6).getPostBlocks().get(7).getContent());
     }
 
     @Test
     void testPostBlockVersionExtraction() {
         // TODO: add test cases for testing the version history extraction (similarity and diffs between code blocks and text blocks, linking of block versions)
 
-        //p_1109108.processVersionHistory();
-        //PostVersion version_7 = p_1109108.get(6);
+        //a_1109108.processVersionHistory();
+        //PostVersion version_7 = a_1109108.get(6);
         //assertEquals(2, version_7.getCodeBlocks().get(0).getPredDiff().size());
         // ....
     }
 
     @Test
     void testReadingPostHistory20991163() {
-        assertEquals(1, p_20991163.size());
+        assertEquals(1, a_20991163.size());
 
-        PostVersion version_1 = p_20991163.get(0);
+        PostVersion version_1 = a_20991163.get(0);
         assertEquals(1, version_1.getPostBlocks().size());
         assertEquals(0, version_1.getTextBlocks().size());
         assertEquals(1, version_1.getCodeBlocks().size());
@@ -161,4 +163,36 @@ class PostVersionHistoryTest {
         System.out.println("version 1: " + version_1.getContent());
     }
 
+    @Test
+    void testRootPostBlockVersionId3758880() {
+        // there are 11 versions of this post
+        assertEquals(11, a_3758880.size());
+
+        PostVersion firstVersion = a_3758880.get(0);
+        assertEquals(6, firstVersion.getPostBlocks().size());
+        // root post blocks of first version must be null
+        for (PostBlockVersion currentPostBlock : firstVersion.getPostBlocks()) {
+            assertEquals(currentPostBlock.getId(), (int)currentPostBlock.getRootPostBlockVersionId());
+        }
+
+        PostVersion secondPostVersion = a_3758880.get(1);
+        assertEquals(4, secondPostVersion.getPostBlocks().size());
+        // first code block of second version has first code block of first version as root post block
+        assertEquals((int)secondPostVersion.getCodeBlocks().get(0).getRootPostBlockVersionId(),
+                firstVersion.getCodeBlocks().get(0).getId());
+        // second code block of second version has third code block of first version as root post block
+        assertEquals((int)secondPostVersion.getCodeBlocks().get(1).getRootPostBlockVersionId(),
+                firstVersion.getCodeBlocks().get(2).getId());
+        // second text block of second version has no predecessor (-> itself as root post block)
+        assertEquals((int)secondPostVersion.getTextBlocks().get(0).getRootPostBlockVersionId(),
+                secondPostVersion.getTextBlocks().get(0).getId());
+
+        PostVersion lastPostVersion = a_3758880.get(a_3758880.size()-1);
+        // first code block of last version still has first code block of first version as root post block
+        assertEquals((int)lastPostVersion.getCodeBlocks().get(0).getRootPostBlockVersionId(),
+                firstVersion.getCodeBlocks().get(0).getId());
+        // first text block of last version has first text block of second version as root post block
+        assertEquals((int)lastPostVersion.getTextBlocks().get(0).getRootPostBlockVersionId(),
+                secondPostVersion.getTextBlocks().get(0).getId());
+    }
 }
