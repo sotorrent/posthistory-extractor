2f3eeb6ce08e0dc1240ce2297e12cb1e1fa9d74d
Sebastian Baltes <research@sbaltes.com>
2017-08-10 08:31:17 +0200

    Further refactored SQL scripts, add scripts for processing SO dump XML

--- db/import-export/import_sotorrent.sql
+++ db/import-export/import_sotorrent.sql
@@ -1,24 +1,6 @@
 USE `stackoverflow17_06`;
 
 SET foreign_key_checks = 0;
-LOAD DATA LOCAL INFILE 'Badges.csv' INTO TABLE `Badges`
-CHARACTER SET utf8mb4
-FIELDS TERMINATED BY ','
-OPTIONALLY ENCLOSED BY '\"'
-ESCAPED BY '\"'
-LINES TERMINATED BY '\n';
-SET foreign_key_checks = 1;
-
-SET foreign_key_checks = 0;
-LOAD DATA LOCAL INFILE 'Comments.csv' INTO TABLE `Comments`
-CHARACTER SET utf8mb4
-FIELDS TERMINATED BY ','
-OPTIONALLY ENCLOSED BY '\"'
-ESCAPED BY '\"'
-LINES TERMINATED BY '\n';
-SET foreign_key_checks = 1;
-
-SET foreign_key_checks = 0;
 LOAD DATA LOCAL INFILE 'PostBlockDiff.csv' INTO TABLE `PostBlockDiff`
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ','
@@ -55,34 +37,7 @@ LINES TERMINATED BY '\n'
 SET foreign_key_checks = 1;
 
 SET foreign_key_checks = 0;
-LOAD DATA LOCAL INFILE 'PostHistory.csv' INTO TABLE `PostHistory`
-CHARACTER SET utf8mb4
-FIELDS TERMINATED BY ','
-OPTIONALLY ENCLOSED BY '\"'
-ESCAPED BY '\"'
-LINES TERMINATED BY '\n';
-SET foreign_key_checks = 1;
-
-SET foreign_key_checks = 0;
-LOAD DATA LOCAL INFILE 'PostLinks.csv' INTO TABLE `PostLinks`
-CHARACTER SET utf8mb4
-FIELDS TERMINATED BY ','
-OPTIONALLY ENCLOSED BY '\"'
-ESCAPED BY '\"'
-LINES TERMINATED BY '\n';
-SET foreign_key_checks = 1;
-
-SET foreign_key_checks = 0;
 LOAD DATA LOCAL INFILE 'PostReferenceGH.csv' INTO TABLE `PostReferenceGH`
-CHARACTER SET utf8mb4
-FIELDS TERMINATED BY ','
-OPTIONALLY ENCLOSED BY '\"'
-ESCAPED BY '\"'
-LINES TERMINATED BY '\n';
-SET foreign_key_checks = 1;
-
-SET foreign_key_checks = 0;
-LOAD DATA LOCAL INFILE 'Posts.csv' INTO TABLE `Posts`
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '\"'
@@ -110,33 +65,6 @@ SET foreign_key_checks = 1;
 
 SET foreign_key_checks = 0;
 LOAD DATA LOCAL INFILE 'PostVersionUrl.csv' INTO TABLE `PostVersionUrl`
-CHARACTER SET utf8mb4
-FIELDS TERMINATED BY ','
-OPTIONALLY ENCLOSED BY '\"'
-ESCAPED BY '\"'
-LINES TERMINATED BY '\n';
-SET foreign_key_checks = 1;
-
-SET foreign_key_checks = 0;
-LOAD DATA LOCAL INFILE 'Tags.csv' INTO TABLE `Tags`
-CHARACTER SET utf8mb4
-FIELDS TERMINATED BY ','
-OPTIONALLY ENCLOSED BY '\"'
-ESCAPED BY '\"'
-LINES TERMINATED BY '\n';
-SET foreign_key_checks = 1;
-
-SET foreign_key_checks = 0;
-LOAD DATA LOCAL INFILE 'Users.csv' INTO TABLE `Users`
-CHARACTER SET utf8mb4
-FIELDS TERMINATED BY ','
-OPTIONALLY ENCLOSED BY '\"'
-ESCAPED BY '\"'
-LINES TERMINATED BY '\n';
-SET foreign_key_checks = 1;
-
-SET foreign_key_checks = 0;
-LOAD DATA LOCAL INFILE 'Votes.csv' INTO TABLE `Votes`
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '\"'
