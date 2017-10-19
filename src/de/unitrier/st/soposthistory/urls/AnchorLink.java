8c5f04811c5b57567cc39569afe01330c6a00977
Lorik Dumani <s4loduma@uni-trier.de>
2017-10-19 12:22:35 +0200

    Replaced URLs from ground truth project where all urls have been tested. Added also URL extractor that parses and normalizes multiple types of urls occuring in SO.

--- src/de/unitrier/st/soposthistory/urls/AnchorLink.java
+++ src/de/unitrier/st/soposthistory/urls/AnchorLink.java
@@ -3,8 +3,6 @@ package de.unitrier.st.soposthistory.urls;
 import java.util.regex.Pattern;
 
 public class AnchorLink extends Link {
-    // TODO: adapt for include second matching group for title
-
     // Example: <a href="http://example.com" title="example">example</a>
-    public static final Pattern regex = Pattern.compile("<a\\s+href\\s*=\\s*\"((?:http|ftp|https)://(?:[\\w_-]+(?:(?:\\.[\\w_-]+)+))(?:[\\w.,@?^=%&:/~+#-]*[\\w@?^=%&/~+#-]))?\"");
+    public static final Pattern regex = Pattern.compile("<a\\s+href\\s*=\\s*\"((?:http|ftp|https):\\/\\/(?:[\\w_-]+(?:(?:\\.[\\w_-]+)+))(?:[\\w.,@?^=%&:\\/~+#-]*[\\w@?^=%&\\/~+#-]))?\"\\s+(title=\"(.*)\")>(.*)<\\/a>");
 }
