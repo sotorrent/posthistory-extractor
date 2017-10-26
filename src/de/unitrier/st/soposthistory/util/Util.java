f733965f56adb2057655f3adfedf2db5077cac1d
Sebastian Baltes <research@sbaltes.com>
2017-10-26 15:24:58 +0200

    Move class logger creation to a utils method

--- src/de/unitrier/st/soposthistory/util/Util.java
+++ src/de/unitrier/st/soposthistory/util/Util.java
@@ -2,7 +2,14 @@ package de.unitrier.st.soposthistory.util;
 
 import org.hibernate.StatelessSession;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class Util {
     public static void insertList(StatelessSession session, List list) {
@@ -15,5 +22,18 @@ public class Util {
         for (int i=0; i<list.size(); i++) {
             session.update(list.get(i));
         }
+    }
+
+    public static Logger getClassLogger(Class c, boolean consoleOutput) throws IOException {
+        Path logFileDir  = Paths.get(System.getProperty("user.dir"));
+        String logFile = Paths.get(logFileDir.toString(), c.getSimpleName() + ".log").toString();
+        Logger logger = Logger.getLogger(c.getName());
+        if (!consoleOutput) {
+            logger.setUseParentHandlers(false); // disable handlers inherited from root logger
+        }
+        Handler fileHandler = new FileHandler(logFile);
+        fileHandler.setFormatter(new SimpleFormatter());
+        logger.addHandler(fileHandler);
+        return logger;
     }
 }
