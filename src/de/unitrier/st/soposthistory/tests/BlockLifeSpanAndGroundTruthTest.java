40baedc8c915e2238a620c9d72d43e11242d8f1c
Lorik Dumani <s4loduma@uni-trier.de>
2017-11-04 00:15:48 +0100

    Added new test case
     * test case parses results (tp, fp, fn but still not tn) from old metric comparison project and compares them with the new one.
     * post with id 3758880 has exactly the same results
     * post with id does not

--- src/de/unitrier/st/soposthistory/tests/BlockLifeSpanAndGroundTruthTest.java
+++ src/de/unitrier/st/soposthistory/tests/BlockLifeSpanAndGroundTruthTest.java
@@ -359,6 +359,115 @@ class BlockLifeSpanAndGroundTruthTest {
         manager.writeToCSV(outputDir);
     }
 
+    @Test
+    void testCompareMetricComparisonManagerWithComparisonFromOldProject() {
+        MetricComparisonManager manager = MetricComparisonManager.create(
+                "TestManager", pathToPostIdList, pathToPostHistory, pathToGroundTruth, true
+        );
+
+        List<Integer> postHistoryIds_3758880 = manager.getPostVersionLists().get(3758880).getPostHistoryIds();
+        List<Integer> postHistoryIds_22037280 = manager.getPostVersionLists().get(22037280).getPostHistoryIds();
+
+        manager.compareMetrics();
+        manager.writeToCSV(Paths.get("testdata","metrics comparison"));
+
+        CSVParser csvParser = null;
+
+        try {
+            csvParser = CSVParser.parse(
+                    Paths.get("testdata", "metrics comparison", "resultsMetricComparisonOldProject.csv").toFile(),
+                    StandardCharsets.UTF_8,
+                    CSVFormat.DEFAULT.withHeader("sample", "metric", "threshold", "postid", "posthistoryid", "runtimetext", "#truepositivestext", "#truenegativestext",
+                            "#falsepositivestext", "#falsenegativestext", "#runtimecode", "#truepositivescode", "#truenegativescode",
+                            "#falsepositivescode", "#falsenegativescode").withDelimiter(';')
+                            .withQuote('"')
+                            .withQuoteMode(QuoteMode.MINIMAL)
+                            .withEscape('\\')
+                            .withFirstRecordAsHeader()
+            );
+
+            csvParser.getHeaderMap();
+            List<CSVRecord> records = csvParser.getRecords();
+            for(CSVRecord record : records){
+                String metric = record.get("metric");
+
+                Double threshold = Double.valueOf(record.get("threshold"));
+                if((int)(threshold*100) % 10 != 0) { // Comparison manager computes only equal thresholds so unequal thresholds will be skipped
+                    continue;
+                }
+
+                Integer postId = Integer.valueOf(record.get("postid"));
+
+                Integer postHistoryId = null;
+
+                Integer truePositivesText = null;
+                Integer trueNegativesText = null;
+                Integer falsePositivesText = null;
+                Integer falseNegativesText = null;
+
+                Integer truePositivesCode = null;
+                Integer trueNegativesCode = null;
+                Integer falsePositivesCode = null;
+                Integer falseNegativesCode = null;
+
+                try{
+                    postHistoryId = Integer.valueOf(record.get("posthistoryid"));
+
+                    truePositivesText = Integer.valueOf(record.get("#truepositivestext"));
+                    trueNegativesText = Integer.valueOf(record.get("#truenegativestext"));
+                    falsePositivesText = Integer.valueOf(record.get("#falsepositivestext"));
+                    falseNegativesText = Integer.valueOf(record.get("#falsenegativestext"));
+
+                    truePositivesCode = Integer.valueOf(record.get("#truepositivescode"));
+                    trueNegativesCode = Integer.valueOf(record.get("#truenegativescode"));
+                    falsePositivesCode = Integer.valueOf(record.get("#falsepositivescode"));
+                    falseNegativesCode = Integer.valueOf(record.get("#falsenegativescode"));
+                }catch (NumberFormatException e){}
+
+                MetricComparison tmpMetricComparison = manager.getMetricComparison(postId, metric, threshold);
+
+
+                if(postHistoryId == null) {
+                    List<Integer> postHistoryIds = null;
+                    if(postId == 3758880) {
+                        postHistoryIds = postHistoryIds_3758880;
+                    }else if(postId == 22037280){
+                        postHistoryIds = postHistoryIds_22037280;
+                    }
+
+                    for (Integer tmpPostHistoryId : postHistoryIds) {
+                        assertNull(tmpMetricComparison.getTruePositivesText().get(tmpPostHistoryId));
+                        assertNull(tmpMetricComparison.getFalsePositivesText().get(tmpPostHistoryId));
+                        assertNull(tmpMetricComparison.getTrueNegativesText().get(tmpPostHistoryId));
+                        assertNull(tmpMetricComparison.getFalseNegativesText().get(tmpPostHistoryId));
+
+                        assertNull(tmpMetricComparison.getTruePositivesCode().get(tmpPostHistoryId));
+                        assertNull(tmpMetricComparison.getFalsePositivesCode().get(tmpPostHistoryId));
+                        assertNull(tmpMetricComparison.getTrueNegativesCode().get(tmpPostHistoryId));
+                        assertNull(tmpMetricComparison.getFalseNegativesCode().get(tmpPostHistoryId));
+                    }
+
+                } else {
+
+                    if(postId != 3758880) // TODO: This test case works only for post 3758880 but not for 22037280
+                        continue;
+
+                    // TODO: Check true negatives for text and code
+                    assertEquals(tmpMetricComparison.getTruePositivesText().get(postHistoryId), truePositivesText);
+                    assertEquals(tmpMetricComparison.getFalsePositivesText().get(postHistoryId), falsePositivesText);
+                    assertEquals(tmpMetricComparison.getFalseNegativesText().get(postHistoryId), falseNegativesText);
+
+                    assertEquals(tmpMetricComparison.getTruePositivesCode().get(postHistoryId), truePositivesCode);
+                    assertEquals(tmpMetricComparison.getFalsePositivesCode().get(postHistoryId), falsePositivesCode);
+                    assertEquals(tmpMetricComparison.getFalseNegativesCode().get(postHistoryId), falseNegativesCode);
+
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Test
     void testGetPossibleConnections() {
