6424be4ac728c491917841e629432b04c916c336
Sebastian Baltes <research@sbaltes.com>
2017-11-21 10:52:35 +0100

    Update BigQuery SQL scripts
    * fix bug in URL normalization
    * add metadata to SO posts

--- db/bigquery/extract_so_references.sql
+++ db/bigquery/extract_so_references.sql
@@ -1,45 +1,58 @@
---- Status: 24.07.2017
+--- Status: 20.11.2017
 --- Execute this in BigQuery
 
 --- TODO: Check extraction of SO references (e.g. post 3758880)
 
 --- Select all source code lines of text files that contain a link to Stack Overflow
 SELECT
-  id,
+  file_id,
   size,
+  copies,
   REGEXP_REPLACE(
-    REGEXP_EXTRACT(LOWER(line), r'(https?://stackoverflow\.com/[^\s)\.\"]*)'),
-    r'(^https)',
-    'http'
+      REGEXP_EXTRACT(LOWER(line), r'(https?://stackoverflow\.com/[^\s)\.\"]*)'),
+      r'(^https)',
+      'http'
   ) as url,
   line
 FROM (
   SELECT
-    id,
+    file_id,
     size,
-    SPLIT(content, '\n') as line
-  FROM [bigquery-public-data:github_repos.contents]
-  WHERE
-    binary = false
-    AND content is not null 
+    copies,
+    line
+  FROM (
+    SELECT
+      id as file_id,
+      size,
+      copies,
+      SPLIT(content, '\n') as lines
+    FROM `bigquery-public-data.github_repos.contents`
+    WHERE
+      binary = false
+      AND content is not null
+  )
+  CROSS JOIN UNNEST(lines) as line  
 )
-WHERE REGEXP_MATCH(line, r'(?i:https?://stackoverflow\.com/[^\s)\.\"]*)');
-=> so_references.matched_lines
+WHERE REGEXP_CONTAINS(line, r'(?i:https?://stackoverflow\.com/[^\s)\.\"]*)');
+
+=> so_references_2017_11_20.matched_lines
 
 
 --- Join with table files to get information about repos
 SELECT
-  files.id as file_id,
+  lines.file_id as file_id,
   repo_name,
   REGEXP_EXTRACT(ref, r'refs/heads/(.+)') as branch,
   path,
   size,
+  copies,
   url,
   line
-FROM [soposthistory:so_references.matched_lines] as lines
-INNER JOIN [bigquery-public-data:github_repos.files] as files
-ON lines.id = files.id;
-=> so_references.matched_files
+FROM `soposthistory.so_references_2017_11_20.matched_lines` as lines
+LEFT JOIN `bigquery-public-data.github_repos.files` as files
+ON lines.file_id = files.id;
+
+=> so_references_2017_11_20.matched_files
 
 
 --- Normalize the SO links to (http://stackoverflow.com/(a/q)/<id>)
@@ -49,18 +62,23 @@ SELECT
   branch,
   path,
   size,
+  copies,
   CASE
-    WHEN REGEXP_MATCH(url, r'(http:\/\/stackoverflow\.com\/(?:a|q)\/[\d]+)')
-    THEN REGEXP_EXTRACT(url, r'http:\/\/stackoverflow\.com\/(?:a|q)\/([\d]+)')
-    WHEN REGEXP_MATCH(url, r'https?:\/\/stackoverflow\.com\/questions\/[\d]+\/[^\s\/\#]+(?:\/|\#)([\d]+)')
-    THEN CONCAT("http://stackoverflow.com/a/", REGEXP_EXTRACT(url, r'https?:\/\/stackoverflow\.com\/questions\/[\d]+\/[^\s\/\#]+(?:\/|\#)([\d]+)'))
-    WHEN REGEXP_MATCH(url, r'(https?:\/\/stackoverflow\.com\/questions\/[\d]+)')
-    THEN CONCAT("http://stackoverflow.com/q/", REGEXP_EXTRACT(url, r'https?:\/\/stackoverflow\.com\/questions\/([\d]+)'))
+    --- DO NOT replace the distinction between answers and questions, because otherwise URLs like this won't be matched: http://stackoverflow.com/a/3758880/1035417
+    WHEN REGEXP_CONTAINS(LOWER(url), r'(https?:\/\/stackoverflow\.com\/a\/[\d]+)')
+    THEN CONCAT("http://stackoverflow.com/a/", REGEXP_EXTRACT(LOWER(url), r'https?:\/\/stackoverflow\.com\/a\/([\d]+)'))
+    WHEN REGEXP_CONTAINS(LOWER(url), r'(https?:\/\/stackoverflow\.com\/q\/[\d]+)')
+    THEN CONCAT("http://stackoverflow.com/q/", REGEXP_EXTRACT(LOWER(url), r'https?:\/\/stackoverflow\.com\/q\/([\d]+)'))
+    WHEN REGEXP_CONTAINS(LOWER(url), r'https?:\/\/stackoverflow\.com\/questions\/[\d]+\/[^\s\/\#]+(?:\/|\#)([\d]+)')
+    THEN CONCAT("http://stackoverflow.com/a/", REGEXP_EXTRACT(LOWER(url), r'https?:\/\/stackoverflow\.com\/questions\/[\d]+\/[^\s\/\#]+(?:\/|\#)([\d]+)'))
+    WHEN REGEXP_CONTAINS(LOWER(url), r'(https?:\/\/stackoverflow\.com\/questions\/[\d]+)')
+    THEN CONCAT("http://stackoverflow.com/q/", REGEXP_EXTRACT(LOWER(url), r'https?:\/\/stackoverflow\.com\/questions\/([\d]+)'))
     ELSE url
   END as url,
   line
-FROM [soposthistory:so_references.matched_files];
-=> so_references.matched_files_normalized
+FROM `soposthistory.so_references_2017_11_20.matched_files`;
+
+=> so_references_2017_11_20.matched_files_normalized
 
 
 --- Extract post id from links, set post type id, and extract file extension from path
@@ -71,20 +89,22 @@ SELECT
   path,
   LOWER(REGEXP_EXTRACT(path, r'(\.[^.]+$)')) as file_ext,
   size,
-  REGEXP_EXTRACT(url, r'http:\/\/stackoverflow\.com\/(?:a|q)\/([\d]+)') as post_id,
+  copies,
+  CAST(REGEXP_EXTRACT(url, r'http:\/\/stackoverflow\.com\/(?:a|q)\/([\d]+)') AS INT64) as post_id,
   CASE
-    WHEN REGEXP_MATCH(url, r'(http:\/\/stackoverflow\.com\/q\/[\d]+)')
+    WHEN REGEXP_CONTAINS(url, r'(http:\/\/stackoverflow\.com\/q\/[\d]+)')
     THEN 1
-    WHEN REGEXP_MATCH(url, r'(http:\/\/stackoverflow\.com\/a\/[\d]+)')
+    WHEN REGEXP_CONTAINS(url, r'(http:\/\/stackoverflow\.com\/a\/[\d]+)')
     THEN 2
     ELSE NULL
   END as post_type_id,
   url,
   line
-FROM [soposthistory:so_references.matched_files_normalized]
+FROM `soposthistory.so_references_2017_11_20.matched_files_normalized`
 WHERE
-  REGEXP_MATCH(url, r'(http:\/\/stackoverflow\.com\/(?:a|q)\/[\d]+)');
-=> so_references.matched_files_aq
+  REGEXP_CONTAINS(url, r'(http:\/\/stackoverflow\.com\/(?:a|q)\/[\d]+)');
+  
+=> so_references_2017_11_20.matched_files_aq
 
 
 --- Use camel case for column names and remove line content for export into MySQL database
@@ -95,10 +115,78 @@ SELECT
   path as Path,
   file_ext as FileExt,
   size as Size,
+  copies as Copies,
   post_id as PostId,
   post_type_id as PostTypeId,
   url as Url
-FROM [soposthistory:so_references.matched_files_aq];
-=> so_references.PostReferenceGH
+FROM `soposthistory.so_references_2017_11_20.matched_files_aq`;
 
-  
+=> so_references_2017_11_20.PostReferenceGH
+
+--- TODO: merge with SO data
+
+--- Retrieve info about referenced SO answers
+WITH
+  answers AS (
+    SELECT
+      FileId,
+      RepoName,
+      Branch,
+      Path,
+      FileExt,
+      Size,
+      Copies,
+      Url,
+      PostId,
+      PostTypeId,
+      comment_count As CommentCount,
+      score as Score,
+      parent_id as ParentId
+    FROM `soposthistory.so_references_2017_11_20.PostReferenceGH` ref
+    LEFT JOIN `bigquery-public-data.stackoverflow.posts_answers` a
+    ON ref.PostId = a.id
+    WHERE PostTypeId=2
+  )
+SELECT 
+  FileId,
+  RepoName,
+  Branch,
+  Path,
+  FileExt,
+  Size,
+  Copies,
+  Url,
+  PostId,
+  PostTypeId,
+  CommentCount,
+  answers.Score as Score,
+  ParentId,
+  view_count as ParentViewCount
+FROM answers
+LEFT JOIN `bigquery-public-data.stackoverflow.posts_questions` q
+ON answers.ParentId = q.id;
+
+=> so_references_2017_11_20.PostReferenceGH_Answers
+
+
+--- Retrieve info about referenced SO questions
+SELECT
+  FileId,
+  RepoName,
+  Branch,
+  Path,
+  FileExt,
+  Size,
+  Copies,
+  Url,
+  PostId,
+  PostTypeId,
+  comment_count As CommentCount,
+  score as Score,
+  view_count as ViewCount
+FROM `soposthistory.so_references_2017_11_20.PostReferenceGH` ref
+LEFT JOIN `bigquery-public-data.stackoverflow.posts_questions` q
+ON ref.PostId = q.id
+WHERE PostTypeId=1;
+
+=> so_references_2017_11_20.PostReferenceGH_Questions
