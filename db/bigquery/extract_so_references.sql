72540036ceda93630b6b13b43a189f823ca5560a
Sebastian Baltes <research@sbaltes.com>
2017-11-23 14:54:23 +0100

    Add BigQuery SQL statements to retrieve SO Java A/Q stats

--- db/bigquery/extract_so_references.sql
+++ db/bigquery/extract_so_references.sql
@@ -190,3 +190,32 @@ ON ref.PostId = q.id
 WHERE PostTypeId=1;
 
 => so_references_2017_11_20.PostReferenceGH_Questions
+
+
+--- Retrieve information about all Java questions to compare referenced questions with all questions
+SELECT
+  id as post_id,
+  REGEXP_CONTAINS(tags, r'(?:^|.+\|)(java)(?:\||$)') as java_tag,
+  REGEXP_CONTAINS(tags, r'(?:^|.+\|)(android)(?:\||$)') as android_tag,
+  score,
+  comment_count,
+  view_count
+FROM `bigquery-public-data.stackoverflow.posts_questions`
+WHERE REGEXP_CONTAINS(tags, r'(?:^|.+\|)(java|android)(?:\||$)');
+
+=> so_java.questions
+
+--- Retrieve information about all Java answers to compare referenced answers with all answers
+SELECT
+  answers.id as post_id,
+  answers.parent_id as parent_id,
+  java_questions.java_tag as java_tag,
+  java_questions.android_tag as android_tag,
+  answers.score as score,
+  answers.comment_count as comment_count,
+  java_questions.view_count as parent_view_count
+FROM `bigquery-public-data.stackoverflow.posts_answers` answers
+INNER JOIN `soposthistory.so_java.questions` java_questions 
+ON java_questions.post_id = answers.parent_id;
+  
+=> so_java.answers
